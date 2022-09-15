3f0c25d478431b235decac4f6e797263647cabb5
cs-mas <masako.gray@clearscore.com>
2022-09-15 14:23:43 +0100

    appending button text to calulator screen

--- app/src/main/java/com/practice/secondcalculator/MainActivity.kt
+++ app/src/main/java/com/practice/secondcalculator/MainActivity.kt
@@ -5,15 +5,23 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var textViewCalcScreen : TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textViewCalcScreen = findViewById(R.id.textViewCalcScreen)
     }
 
+
     fun onDigit(view: View){
-        Toast.makeText(this, "Button Clicked", Toast.LENGTH_LONG).show()
+//        /Toast.makeText(this, "Button Clicked", Toast.LENGTH_LONG).show()
+        textViewCalcScreen?.append((view as Button).text)
     }
 }
